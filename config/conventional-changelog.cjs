7c13f1d5cb8360af583520d63c6511b8cfd25af6
Markus Aldenhövel <maldenhoevel@gmail.com>
2026-01-25 20:51:35 +0100

    chore: use local changelog preset

---
+++ config/conventional-changelog.cjs
@@ -0,0 +1,32 @@
+const TYPE_TITLES = {
+  feat: "Features",
+  fix: "Bug Fixes",
+  perf: "Performance Improvements",
+  refactor: "Refactors",
+  docs: "Documentation",
+  test: "Tests",
+  chore: "Chores",
+};
+
+module.exports = () => ({
+  parserOpts: {
+    headerPattern: /^(\w*)(?:\((.*)\))?: (.*)$/,
+    headerCorrespondence: ["type", "scope", "subject"],
+    noteKeywords: ["BREAKING CHANGE", "BREAKING CHANGES"],
+  },
+  writerOpts: {
+    groupBy: "type",
+    commitGroupsSort: "title",
+    commitsSort: ["scope", "subject"],
+    transform: (commit) => {
+      if (!commit.type) {
+        return commit;
+      }
+      const title = TYPE_TITLES[commit.type];
+      return {
+        ...commit,
+        type: title || commit.type,
+      };
+    },
+  },
+});
